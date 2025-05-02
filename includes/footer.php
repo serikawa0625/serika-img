c97a38b3037c8ebd085ed248bb82d4e0cc0f54ef
Serikawa <yz001219@977958.xyz>
2025-05-02 23:20:16 +0800

    页脚文件
    页脚文件老朋友

--- includes/footer.php
+++ includes/footer.php
@@ -3,7 +3,7 @@
             <div class="footer-content">
                 <div class="footer-section">
                     <h4>关于我的图床</h4>
-                    <p>原创作者github serikaWa</p>
+                    <p>原创作者github Serikawa</p>
                     <p>原创图床，纯手搓，参考了很多资料与样式，要累死了>_<</p>
                     <p>提供图片托管服务</p>
                     <p>最大支持5MB的图片文件，有能力请弄到最小</p>
@@ -16,7 +16,7 @@
                 <div class="footer-section">
                     <h4>技术支持</h4>
                     <p>Email: info@977958.xyz</p> /改成你自己的
-                    <p>图床来源GitHub: github.com/serika-img</p> //别删这段谢谢
+                    <p>图床来源GitHub: https://github.com/serikawa0625/serika-img</p> //别删这段谢谢
                 </div>
             </div>
             <div class="copyright">
